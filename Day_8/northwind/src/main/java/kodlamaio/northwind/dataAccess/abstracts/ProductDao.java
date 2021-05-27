3f3cffdc802ba23c3bfa3aba106d193feba8c340
Mustafa ÜNLÜ <73909923+codeunlu@users.noreply.github.com>
2021-05-27 23:03:11 +0300

    Add files via upload

---
+++ Day_8/northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -0,0 +1,37 @@
+package kodlamaio.northwind.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kodlamaio.northwind.entities.concretes.Product;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductDao extends JpaRepository<Product, Integer>{
+    // where productname = productname
+    Product getByProductName(String productName);
+
+    // where komutunda ve
+    Product getByProductNameAndCategory(String productName, int categoryId);
+
+    // where komutunda veya
+    List<Product> getByProductNameOrCategory(String productName, int categoryId);
+
+    // categories id leri varsa (in komutu)
+    List<Product> getByCategoryIn(List<Integer> categories);
+
+    // İsmin içinde name içeriyorsa
+    List<Product> getByProductNameContains(String productName);
+
+    // Bu isimle başlayanları getir
+    List<Product> getByProductNameStartingWith(String productName);
+
+    // Kendimiz query yazmak istersek (JPQL)
+    // Entitiy de yer alan alana göre yapılır
+    @Query("From Product where productName=:productName and category.categoryId=:categoryId")
+    List<Product> getByNameAndCategory(String productName, int categoryId);
+
+
+	
+}
+
