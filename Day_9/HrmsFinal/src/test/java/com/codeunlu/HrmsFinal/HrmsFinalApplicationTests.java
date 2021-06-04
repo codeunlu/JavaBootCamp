f069ad27f979a735c025410ebfa863613122d71e
Mustafa ÜNLÜ <73909923+codeunlu@users.noreply.github.com>
2021-06-05 02:31:10 +0300

    Add files via upload

---
+++ Day_9/HrmsFinal/src/test/java/com/codeunlu/HrmsFinal/HrmsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeunlu.HrmsFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
